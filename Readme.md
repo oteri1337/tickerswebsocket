c0d4e69cbe3d161661d97013fcaf7067415207c4
Oteri <oteri4u@gmail.com>
2020-10-07 10:48:25 +0100

    hitbtc support added

--- Readme.md
+++ Readme.md
@@ -1,3 +1,13 @@
 Node JS Websocket for live crypto, stocks and forex prices
 
 endpoint ws://tickerswebsocket.herokuapp.com
+
+# Finihub Supported Symbols
+
+https://finnhub.io/api/v1/crypto/symbol?exchange=binance&token=btunjb748v6q25697fng
+
+https://finnhub.io/api/v1/crypto/symbol?exchange=hitbtc&token=btunjb748v6q25697fng
+
+# Finihub Supported Stocks
+
+https://finnhub.io/api/v1/stock/symbol?exchange=US&token=btunjb748v6q25697fng
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const sServer = new WebSocketServer({ port: process.env.PORT || 1027 });
 function runSocket() {
   console.log("running socket");
 
-  let socket = new WebSocket("wss://ws.finnhub.io?token=btg5pp748v6r32agac1g");
+  let socket = new WebSocket("wss://ws.finnhub.io?token=btunjb748v6q25697fng");
 
   function subscribe(symbol) {
     return JSON.stringify({ type: "subscribe", symbol });
@@ -16,25 +16,50 @@ function runSocket() {
 
   // Connection opened -> Subscribe
   socket.addEventListener("open", function (event) {
-    // cryptos
-    socket.send(subscribe("BINANCE:BTCUSDT"));
-    socket.send(subscribe("BINANCE:ETHUSDT"));
-    socket.send(subscribe("BINANCE:TRXUSDT"));
-    socket.send(subscribe("BINANCE:XRPUSDT"));
-    socket.send(subscribe("BINANCE:ZECUSDT"));
-    socket.send(subscribe("BINANCE:LTCUSDT"));
-    socket.send(subscribe("BINANCE:DASHUSDT"));
-    socket.send(subscribe("BINANCE:NEOUSDT"));
-    socket.send(subscribe("BINANCE:QTUMUSDT"));
-    socket.send(subscribe("BINANCE:IOTAUSDT"));
-    socket.send(subscribe("BINANCE:ADAUSDT"));
-    socket.send(subscribe("BINANCE:ONTUSDT"));
-    socket.send(subscribe("BINANCE:OMGUSDT"));
-    socket.send(subscribe("BINANCE:BNBUSDT"));
-    socket.send(subscribe("BINANCE:XLMUSDT"));
-    //
+    // cryptos - hitbtc;
+    socket.send(subscribe("HITBTC:BTCUSD"));
+    socket.send(subscribe("HITBTC:ETHUSD"));
+    socket.send(subscribe("HITBTC:TRXUSD"));
+    socket.send(subscribe("HITBTC:XRPUSD"));
+    socket.send(subscribe("HITBTC:ZECUSD"));
+    socket.send(subscribe("HITBTC:LTCUSD"));
+    socket.send(subscribe("HITBTC:NEOUSD"));
+    socket.send(subscribe("HITBTC:ADAUSD"));
+    socket.send(subscribe("HITBTC:ONTUSD"));
+    socket.send(subscribe("HITBTC:OMGUSD"));
+    socket.send(subscribe("HITBTC:BNBUSD"));
+    socket.send(subscribe("HITBTC:XLMUSD"));
+    socket.send(subscribe("HITBTC:EOSUSD"));
+    socket.send(subscribe("HITBTC:DASHUSD"));
+    socket.send(subscribe("HITBTC:QTUMUSD"));
+    socket.send(subscribe("HITBTC:IOTAUSD"));
+
+    // cryptos - binance
+    // socket.send(subscribe("BINANCE:BTCUSDT"));
+    // socket.send(subscribe("BINANCE:ETHUSDT"));
+    // socket.send(subscribe("BINANCE:TRXUSDT"));
+    // socket.send(subscribe("BINANCE:XRPUSDT"));
+    // socket.send(subscribe("BINANCE:ZECUSDT"));
+    // socket.send(subscribe("BINANCE:LTCUSDT"));
+    // socket.send(subscribe("BINANCE:NEOUSDT"));
+    // socket.send(subscribe("BINANCE:ADAUSDT"));
+    // socket.send(subscribe("BINANCE:ONTUSDT"));
+    // socket.send(subscribe("BINANCE:OMGUSDT"));
+    // socket.send(subscribe("BINANCE:BNBUSDT"));
+    // socket.send(subscribe("BINANCE:XLMUSDT"));
+    // socket.send(subscribe("BINANCE:EOSUSDT"));
+    // socket.send(subscribe("BINANCE:DASHUSDT"));
+    // socket.send(subscribe("BINANCE:QTUMUSDT"));
+    // socket.send(subscribe("BINANCE:IOTAUSDT"));
 
     // stocks
+    socket.send(subscribe("AAPL"));
+    socket.send(subscribe("AMZN"));
+    socket.send(subscribe("MSFT"));
+    socket.send(subscribe("BA"));
+    socket.send(subscribe("CEMB"));
+    socket.send(subscribe("SQ"));
+    socket.send(subscribe("BABA"));
     socket.send(subscribe("V"));
     socket.send(subscribe("MA"));
     socket.send(subscribe("FB"));
@@ -42,15 +67,14 @@ function runSocket() {
     socket.send(subscribe("NKE"));
     socket.send(subscribe("AMD"));
     socket.send(subscribe("ADBE"));
-    socket.send(subscribe("AMZN"));
-    socket.send(subscribe("AAPL"));
     socket.send(subscribe("TSLA"));
     socket.send(subscribe("NVDA"));
     socket.send(subscribe("NLFX"));
     socket.send(subscribe("TWTR"));
     socket.send(subscribe("PYPL"));
-    socket.send(subscribe("MSFT"));
+    socket.send(subscribe("GOOG"));
     socket.send(subscribe("GOOGL"));
+    socket.send(subscribe("TWLO"));
 
     // forex - aud
     socket.send(subscribe("OANDA:AUD_USD"));
@@ -82,19 +106,21 @@ function runSocket() {
     data = JSON.parse(data);
 
     if (data.type == "trade") {
-      const { s } = data.data[0];
+      const { s, p } = data.data[0];
+
+      console.log(s, p);
 
       if (s.startsWith("BINANCE")) {
         const symbol = s.substring(8, s.length);
         finnihubdata[symbol] = data.data[0].p;
-        console.log(symbol, data.data[0].p);
       } else if (s.startsWith("OANDA")) {
         const symbol = s.substring(6, s.length).replace("_", "");
         finnihubdata[symbol] = data.data[0].p;
-        console.log(symbol, data.data[0].p);
+      } else if (s.startsWith("HITBTC")) {
+        const symbol = s.substring(7, s.length) + "T";
+        finnihubdata[symbol] = data.data[0].p;
       } else {
         finnihubdata[s] = data.data[0].p;
-        console.log(s, data.data[0].p);
       }
 
       sServer.clients.forEach((client) => {
@@ -109,15 +135,11 @@ function runSocket() {
     setTimeout(() => {
       runSocket();
     }, 3000);
-    //console.log(socket);
-    // socket = new WebSocket("wss://ws.finnhub.io?token=btg5pp748v6r32agac1g");
-    // console.log(socket);
   });
 
   // error
   socket.addEventListener("error", (e) => {
     console.log("error occured ", e);
-    socket = new WebSocket("wss://ws.finnhub.io?token=btg5pp748v6r32agac1g");
   });
 }
 
